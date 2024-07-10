e0bc691c449a58f91ec29347bf629460939a6787
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-07-10 15:59:22 -0700

    task: doing things in tranches

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,8 @@
+-- Script Lists all bands with Glam rock AS their main style 
+-- list is ranked by their longevity
+
+SELECT band_name,(ifnull(split,2022) - ifnull(formed,0)) lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC
+
